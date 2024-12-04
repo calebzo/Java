bfba18c36dcbccd15b6b7085a4a10e5d177f7bee
calebzo <basseyedubio@gmail.com>
2024-12-04 15:32:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// first alert
+alert("Hello World");
+
+// this is second alert how to write a comment
+alert("How are You");
+
+let myName = "Bryan";
+let myAge = 16;
+
+if ("myAge >= 18") {
+    console.log(myName + ", you are eligible to vote")
+} else {
+    console.log(myName + ", you are not eligible to vote")
+}
+
+let balance = 20000
+let withdrawal = 8000
+
+if (withdrawal <= balance) {
+    console.log("Transaction successful")
+} else {
+    console.log("Transaction failed")
+}
+
