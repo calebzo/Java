2aab432a9f88c7743a847fd715928296430af57c
calebzo <basseyedubio@gmail.com>
2025-01-10 13:45:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,24 @@ if (withdrawal <= balance) {
     console.log("Transaction failed")
 }
 
+// Day 2 
+let age = 25;
+let name = 'Bassey';
+let email = 'basseyedubio@gmail.com';
+
+age = 40;
+
+const phone = '08121970016'
+
+console.log(`Name: ${name} \nEmail: ${email} \nAge ${age} \nPhone Number ${phone}`)
+
+console.log(10 === 10) // true (strict equality)
+console.log(10 !== 5)  // true (not equal)
+console.log(10 > 5)    // true
+console.log(10 < 5)   // false
+console.log(10 >= 5)  // true 
+console.log(10 <= 5)  // false
+
+for (let i = 2; i <= 10; i++) {
+    console.log("Count:" + i);
+}
